98d586e4c4cff47e6489ecec06317b7d2385bbec
Dingzhillio <dingzhillio@gmail.com>
2024-03-12 05:20:37 -0700

    vercel,json

--- server.js
+++ server.js
@@ -60,6 +60,6 @@ app.use('/', mainRoute);
 const PORT = process.env.PORT || 4000;
 app.listen(
     PORT, console.log(
-        `Server running in ${process.env.NODE_ENV}mode on port http://localhost:${PORT}`
+        `Server running in ${process.env.NODE_ENV} mode on port http://localhost:${PORT}`
     )
 )
